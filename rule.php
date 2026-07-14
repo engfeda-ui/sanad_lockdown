d0fe181be58bddf1d46a11d52689f5eb2532b9ce
fody-dev <mahmoud.feda@gmail.com>
2026-07-14 15:30:18 +0300

    style: fix undefined method crash, use standard secure layout, float navigation block fixed on right

--- rule.php
+++ rule.php
@@ -383,8 +383,7 @@ class quizaccess_sanad_lockdown extends quiz_access_rule_base {
      */
     public function setup_attempt_page($page) {
         if (token_manager::is_sanad_browser_request()) {
-            $page->set_pagelayout('standard');
-            $page->set_show_blocks(true);
+            $page->set_pagelayout('secure');
             $page->add_body_class('sanad-secure-kiosk');
         }
     }
